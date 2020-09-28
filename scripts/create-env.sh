1db7f869b23c0459a96018cacd7d29b40518aa5b
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2020-09-28 22:15:19 +0800

    added script to create a .env file
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

---
+++ scripts/create-env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cat << EOF > .env
+  APP_ENV='dev'
+  APP_PORT=9001
+  ENABLED_PREFORK='false'
+  DB_HOST='database'
+  POSTGRES_DB='wryteup_dev'
+  POSTGRES_USER='postgres'
+  POSTGRES_PASSWORD='postgres'
+EOF
+
