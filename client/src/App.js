88ca75223da7abc3f2fc589abe0eafc678ada3eb
rbo13 <richard.burk@ifscapital.com.sg>
2020-09-30 13:52:53 +0800

    initial UI setup using grommet

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Heading,
+  Grommet,
+  ResponsiveContext,
+} from 'grommet';
+import { Notification } from 'grommet-icons';
+
+/*
+colors:
+  Rich Black = #04080F
+  Glaucous = #507DBC
+  Baby Blue Eyes = #A1C6EA
+  Beau Blue = #BBD1EA
+  Gainsboro = #DAE3E5
+*/
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#04080F',
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '18px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='small'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
 
 function App() {
+  const [showSidebar, setShowSidebar] = React.useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} full>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar>
+              <Heading level='3' margin='none'>wryteup</Heading>
+              <Button
+                icon={<Notification />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+              <Box flex align='center' justify='center'>
+                <main>
+                  Main body
+                </main>
+              </Box>
+            </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
   );
 }
 
